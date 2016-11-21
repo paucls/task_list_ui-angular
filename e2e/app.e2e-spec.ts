23dd26bb82af0b98ce393725c189ada7022f08f8
angular-cli <angular-cli@angular.io>
2016-11-21 22:05:34 +0000

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { TaskListUiAngular2Page } from './app.po';
+
+describe('task-list-ui-angular2 App', function() {
+  let page: TaskListUiAngular2Page;
+
+  beforeEach(() => {
+    page = new TaskListUiAngular2Page();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
