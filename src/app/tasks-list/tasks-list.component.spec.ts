93f1893a4b327f1282c0e6faca4e0ebf2c6d0da8
Paulo Clavijo Esteban <paucls@gmail.com>
2016-11-26 12:03:06 +0000

    Task list template uses ngFor to list tasks in component.

--- src/app/tasks-list/tasks-list.component.spec.ts
+++ src/app/tasks-list/tasks-list.component.spec.ts
@@ -8,6 +8,8 @@ import { TasksListComponent } from './tasks-list.component';
 describe('TasksListComponent', () => {
   let component: TasksListComponent;
   let fixture: ComponentFixture<TasksListComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,10 +21,23 @@ describe('TasksListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TasksListComponent);
     component = fixture.componentInstance;
+
+    // query for the list-group by CSS element selector
+    de = fixture.debugElement.query(By.css('div.list-group'));
+    el = de.nativeElement;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display the list of tasks', () => {
+    let deItems = de.queryAll(By.css('.list-group-item'));
+
+    expect(deItems.length).toBe(5);
+    expect(deItems[0].nativeElement.textContent).toContain('Buy milk');
+  });
+
 });
--- src/app/tasks-list/tasks-list.component.ts
+++ src/app/tasks-list/tasks-list.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksListComponent implements OnInit {
 
+  tasks: [any] = [
+    {id: 'task-1', name: 'Buy milk', done: false, userId: 'user-1'},
+    {id: 'task-2', name: 'Pay rent', done: true, userId: 'user-1'},
+    {id: 'task-3', name: 'Return book', done: false, userId: 'user-1'},
+    {id: 'task-4', name: 'Clean car', done: true, userId: 'user-1'},
+    {id: 'task-5', name: 'Go running', done: true, userId: 'user-1'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
