959d6c5d266884d7be7cde6f6ad278feaeea5c17
Paulo Clavijo Esteban <paucls@gmail.com>
2016-11-29 07:15:37 +0000

    Unit test TasksService makes http call to API

--- src/app/tasks-list/tasks.service.spec.ts
+++ src/app/tasks-list/tasks.service.spec.ts
@@ -1,23 +1,49 @@
+import {TestBed, inject, tick, fakeAsync} from '@angular/core/testing';
+import {BaseRequestOptions, Http, ConnectionBackend, Response, ResponseOptions} from '@angular/http';
+import {MockBackend} from '@angular/http/testing';
+
 import {TasksService} from './tasks.service';
-import {Http} from '@angular/http';
 
 describe('TasksService', () => {
 
-  let httpFake = {};
-  let tasksService: TasksService;
+  const TASKS = [
+    {id: 'task-1', name: 'Buy milk', done: false, userId: 'user-1'},
+    {id: 'task-2', name: 'Pay rent', done: true, userId: 'user-1'}
+  ];
 
   beforeEach(() => {
-    tasksService = new TasksService(httpFake as Http);
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: Http, useFactory: (backend: ConnectionBackend, defaultOptions: BaseRequestOptions) => {
+          return new Http(backend, defaultOptions);
+        }, deps: [MockBackend, BaseRequestOptions]
+        },
+        {provide: MockBackend, useClass: MockBackend},
+        {provide: BaseRequestOptions, useClass: BaseRequestOptions},
+        {provide: TasksService, useClass: TasksService}
+      ]
+    });
   });
 
   describe('getTasks()', () => {
 
-    // it('should return all tasks', done => {
-    //   tasksService.getTasks().then(tasks => {
-    //     expect(tasks.length).toBe(5);
-    //     done();
-    //   });
-    // });
+    it('should return all tasks', inject([TasksService, MockBackend], fakeAsync((tasksService: TasksService, mockBackend: MockBackend) => {
+      let result;
+
+      mockBackend.connections.subscribe(c => {
+        expect(c.request.url).toBe('http://paucls-task-list-api.herokuapp.com/tasks');
+        let response = new ResponseOptions({body: TASKS});
+        c.mockRespond(new Response(response));
+      });
+
+      tasksService.getTasks().then(tasks => {
+        result = tasks;
+      });
+      tick();
+
+      expect(result.length).toBe(TASKS.length);
+    })));
 
   });
 
