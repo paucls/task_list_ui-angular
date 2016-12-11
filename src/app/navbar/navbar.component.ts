58f22245e60dca60676f0b36bdcd0f70012edbfc
Paulo Clavijo Esteban <paucls@gmail.com>
2016-12-11 11:50:24 +0000

    Fixed navbar background color.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  templateUrl: './navbar.component.html'
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 
