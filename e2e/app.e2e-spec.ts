70d39f0a2911ad33f47e0cc536a12eba28245e50
Paulo Clavijo Esteban <paucls@gmail.com>
2017-01-09 22:36:57 +0000

    Added e2e tests for add, delete and update task.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -11,7 +11,7 @@ describe('Tasks List App', function () {
   describe('Navbar', () => {
 
     it('should display the app name', () => {
-      expect(page.getNavbarBrand()).toEqual('Tasks List');
+      expect(page.navbarBrand.getText()).toEqual('Tasks List');
     });
 
   });
@@ -19,7 +19,48 @@ describe('Tasks List App', function () {
   describe('Tasks list', function () {
 
     it('should have 2 visible tasks', function () {
-      expect(page.getTasks().count()).toBe(2);
+      expect(page.tasks.count()).toBe(2);
+      expect(page.getTask(0).getText()).toBe('Buy milk');
+      expect(page.getTask(1).getText()).toBe('Pay rent');
+    });
+
+    describe('Add task', () => {
+
+      it('should add new task to the list', () => {
+        const newTask = 'Clean the house';
+
+        page.addTask(newTask);
+
+        expect(page.tasks.count()).toBe(3);
+        expect(page.getTask(2).getText()).toBe(newTask);
+      });
+
+    });
+
+    describe('Check task', () => {
+
+      it('should mark task as done when undone task clicked', () => {
+        page.checkTask(0);
+
+        expect(page.isTaskDone(0)).toBe(true);
+      });
+
+      it('should mark task as undone when done task clicked', () => {
+        page.checkTask(1);
+
+        expect(page.isTaskDone(1)).toBe(false);
+      });
+
+    });
+
+    describe('Delete task', () => {
+
+      it('should remove the task from the list', () => {
+        page.deleteTask(0);
+
+        expect(page.tasks.count()).toBe(1);
+      });
+
     });
 
   });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,15 +1,37 @@
-import { browser, element, by } from 'protractor';
+import { browser, element, by, Key } from 'protractor';
 
 export class TaskListUiAngular2Page {
+
+  navbarBrand = element(by.css('.navbar-brand'));
+  addTaskInput = element(by.css('.task-form input'));
+  tasks = element.all(by.css('app-task-detail'));
+
   navigateTo() {
     return browser.get('/');
   }
 
-  getNavbarBrand() {
-    return element(by.css('.navbar-brand')).getText();
+  getTask(index) {
+    return this.tasks.get(index);
   }
 
-  getTasks() {
-    return element.all(by.css('app-task-detail'));
+  addTask(name) {
+    this.addTaskInput.click();
+    this.addTaskInput.sendKeys(name);
+    this.addTaskInput.sendKeys(Key.ENTER);
   }
+
+  deleteTask(index) {
+    this.getTask(index).click();
+    return this.getTask(index).element(by.css('.task-delete')).click();
+  }
+
+  checkTask(index) {
+    return this.getTask(index).element(by.css('.task-check')).click();
+  }
+
+  isTaskDone(index) {
+    return this.getTask(index)
+      .element(by.css('.fa-check-square-o')).isPresent();
+  }
+
 }
